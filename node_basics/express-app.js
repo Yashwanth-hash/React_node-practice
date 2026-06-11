e030cfba061ef1fc7530212fb7f2e774f95d35cf
Yashwanth-Hash <183641464+Yashwanth-hash@users.noreply.github.com.>
2026-06-11 14:19:38 +0530

    11th commit

--- node_basics/express-app.js
+++ node_basics/express-app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{ 
     res.send("Home Page");
 });
+
+app.get("/contact",(req,res)=>{
+    res.json({
+    name: "Y",
+    age: 20
+  });
+});
+
+app.get("/about",(req,res)=>{
+    res.send("About page");
+});
+
 
 app.listen(3000);
 
